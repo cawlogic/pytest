d5f90f8165ee3213ab8a4047d8e95b3d67c35371
Craig Warren <craigwarren@protonmail.com>
2021-06-20 18:59:37 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def func(args):
+    print(args)
+
+
+func('hello')
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pytest',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='tester',
+    author_email='test@testemail.com',
+    description='Just a test dist'
+)
